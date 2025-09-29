3639ab2a8a5aeb6007aa4281abc9873372455432
Azzam <you@example.com>
2025-09-29 12:50:48 +0300

    solve lab5 for css styling

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -1,18 +1,16 @@
 from django.urls import path
 from . import views
 
-# urlpatterns = [
-#     # path('', views.index),
-#     path('', views.index, name="books.index"),
-#     path('index2/<int:val1>/', views.index2),
-#     path('<int:bookId>/', views.viewbook),
-#     path('list_books/', views.list_books, name="books.list_books"),
-#     path('aboutus/', views.aboutus, name="books.aboutus"),
-# ]
+
 
 urlpatterns = [
     path('', views.index, name="books.index"),
     path('list_books/', views.list_books, name="books.list_books"),
     path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
     path('aboutus/', views.aboutus, name="books.aboutus"),
+    # lab 5
+    path('html5/text/formatting', views.text_formatting_view, name='text_formatting_page'),
+    path('html5/listing', views.listing_page_view, name='listing_page'),
+    path('html5/tables', views.tables_page_view, name='tables_page'),
+    path('html5/links', views.links_page_view, name='links_page'),
 ]
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -8,17 +8,6 @@ def index(request):
 def index2(request, val1=0):
     return HttpResponse("value1 = " + str(val1))
 
-# def viewbook(request, bookId):
-    # book1 = {'id': 123, 'title': 'Continuous Delivery', 'author': 'J. Humble and D. Farley'}
-    # book2 = {'id': 456, 'title': 'Secrets of Reverse Engineering', 'author': 'E. Eilam'}
-    # targetBook = None
-    # if book1['id'] == bookId:
-    #     targetBook = book1
-    # if book2['id'] == bookId:
-    #     targetBook = book2
-    # context = {'book': targetBook}
-    # return render(request, 'bookmodule/show.html', context)
-
 def viewbook(request, bookId):
     books = {
         123: {'id': 123, 'title': 'Continuous Delivery', 'author': 'J. Humble and D. Farley', 'image': 'book1.jpg'},
@@ -54,11 +43,21 @@ def list_books(request):
 def index(request):
     return render(request, "bookmodule/index.html")
 
-# def list_books(request):
-#     return render(request, "bookmodule/list_books.html")
-
 def viewbook(request, bookId):
     return render(request, "bookmodule/one_book.html")
 
 def aboutus(request):
     return render(request, "bookmodule/aboutus.html")
+
+
+def links_page_view(request):
+    return render(request, 'bookmodule/links.html')
+
+def text_formatting_view(request):
+    return render(request, 'bookmodule/text_formatting.html')
+
+def listing_page_view(request):
+    return render(request, 'bookmodule/listing.html')
+
+def tables_page_view(request):
+    return render(request, 'bookmodule/tables.html')
